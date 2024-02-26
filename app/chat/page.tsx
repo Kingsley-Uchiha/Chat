c2caab349cb6b3987a2a56ad2c4d0a9c95971d8e
Ibhasebhor Kingsley <ibhasebhorkingsley@gmail.com>
2024-02-27 00:16:53 +0100

    Fix chat bugs

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,13 +2,9 @@ import ChatApps from "@/components/chat apps/page";
 import ChatView from "@/components/chat view/page";
 import Chats from "@/components/chats/page";
 
-export default function Chat(props: {
-  children: React.ReactNode;
-  analytics: React.ReactNode;
-  team: React.ReactNode;
-}) {
+export default function Chat() {
   return (
-    <main className="grid grid-cols-12 gap-4 ">
+    <main className="grid grid-cols-12 gap-4">
       <ChatApps />
       <Chats />
       <ChatView />
--- components/chat apps/page.tsx
+++ components/chat apps/page.tsx
@@ -7,15 +7,15 @@ export default function ChatApps() {
         <Image src="/C.png" width={50} height={50} alt="Chat" />
       </div>
       <div className="chat-app">
-        {/* <Image src="/C.png" width={50} height={50} alt="Chat" /> */}
+        <Image src="/C.png" width={50} height={50} alt="Chat" />
         <h1>F</h1>
       </div>
       <div className="chat-app">
-        {/* <Image src="/C.png" width={50} height={50} alt="Chat" /> */}
+        <Image src="/C.png" width={50} height={50} alt="Chat" />
         <h1>X</h1>
       </div>
       <div className="chat-app">
-        {/* <Image src="/C.png" width={50} height={50} alt="Chat" /> */}
+        <Image src="/C.png" width={50} height={50} alt="Chat" />
         <h1>IG</h1>
       </div>
     </div>
